9113a8caf63aa2ec46689216d9a3ebd77cb1ede8
Rakhmatullah Yoga S <13512053@std.stei.itb.ac.id>
2015-02-07 22:50:13 +0700

    tutorial 1 finished

--- AndroidLifeCycle/app/src/main/java/com/vogella/android/lifecycle/activity/SecondActivity.java
+++ AndroidLifeCycle/app/src/main/java/com/vogella/android/lifecycle/activity/SecondActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class SecondActivity extends TracerActivity {
@@ -12,6 +14,11 @@ public class SecondActivity extends TracerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        String[] values = getResources().getStringArray(R.array.operating_systems);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        spinner.setAdapter(adapter);
     }
 
 }
